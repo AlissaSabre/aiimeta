d8a6c498f5537b962ca05e3bef3492cfa7fa28b3
Alissa Sabre <alissasabre@gmail.com>
2026-02-14 22:08:25 +0900

    Workaround for a symptom that a dropped CFSTR_INETURLW data is sometimes received as a string object. (I don't know why so...)

--- aiimeta/UI/ClipboardHelper.cs
+++ aiimeta/UI/ClipboardHelper.cs
@@ -8,19 +8,25 @@ namespace aiimeta.UI
     /// <summary>Provides several helper methods for use with Clipboard operations.</summary>
     public static class ClipboardHelper
     {
-        /// <summary>Reads a C WSTR data (UTF-16LE text terminated by a '\0') from a MemoryStream.</summary>
-        /// <param name="obj"><see cref="MemoryStream"/> instance.</param>
-        /// <exception cref="InvalidCastException"><paramref name="obj"/> is not a <see cref="MemoryStream"/>.</exception>
+        /// <summary>Reads a C WSTR data from a MemoryStream.</summary>
+        /// <param name="obj"><see cref="string"/> or <see cref="MemoryStream"/> instance.</param>
+        /// <exception cref="InvalidCastException"><paramref name="obj"/> is neither <see cref="string"/> nor <see cref="MemoryStream"/>.</exception>
         /// <exception cref="ArgumentNullException"><paramref name="obj"/> is null.</exception>
         /// <remarks>
         /// This method is intended for use like <c>e.Data.GetData("format")?.AsString()</c>
         /// in a <see cref="System.Windows.UIElement.Drop"/> event hander
-        /// for a WSTR data format that the WPF doesn't know.
-        /// As a side effect, this method invokes <see cref="IDisposable.Dispose"/> 
-        /// for <paramref name="obj"/>.
+        /// for a C WSTR data format that the WPF doesn't automatically convert.
+        /// When WPF auto-convert the data to string, this method returns it.
+        /// (C WSTR data is a UTF-16LE data stream with a terminating '\0' (U+0000).)
+        /// </remarks>
+        /// <remarks>
+        /// If <paramref name="obj"/> is <see cref="MemoryStream"/>, 
+        /// this method invokes its <see cref="IDisposable.Dispose"/> 
+        /// as a side effect.
         /// </remarks>
         public static string AsString(this object obj)
         {
+            if (obj is string str) return str;
             var m = (MemoryStream)obj ?? throw new ArgumentNullException(nameof(obj));
             using var r = new StreamReader(m, Encoding.Unicode);
             var s = r.ReadToEnd();
